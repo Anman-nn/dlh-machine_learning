7ae1693bd93c713e8ddbfa77c69912b35960e23f
Anman-nn <endricos@gmail.com>
2026-06-06 21:23:58 +0200

    task new

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Documentatio of the module"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """sjkdhkjsdhfdsjkhf"""
+    res = mat1.copy()
+    if axis == 0:    
+        for row_m2 in mat2:
+            if len(row_m2) != len(res[0]):
+                return None
+            res.append(row_m2)
+        return res
+
+    if axis == 1:
+        for i in range(len(mat1)):
+            res[i] += mat2[i]
+        return res
