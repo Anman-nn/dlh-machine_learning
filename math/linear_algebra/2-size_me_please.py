9e08144a02557e78682f91905dac293467f88d8a
Anman-nn <endricos@gmail.com>
2026-06-06 09:47:52 +0200

    task linear algebra

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """Returns the sixe of any dimentional matrix"""
     x = []
     while isinstance(matrix, list):
         x.append(len(matrix))
         if not matrix:
             break
-        matrix = matrix[0]  
+        matrix = matrix[0]
     return x
