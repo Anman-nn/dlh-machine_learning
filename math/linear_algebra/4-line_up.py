44806352ca5277abb477cf4a5a57bb714d384563
Anman-nn <endricos@gmail.com>
2026-06-06 20:24:36 +0200

    task new

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Documentatio of the module"""
+
+
+def add_arrays(arr1, arr2):
+    '''def add_arrays(arr1, arr2)'''
+    if len(arr1) != len(arr2):
+        return None
+    return [a + b for a,b in zip(arr1, arr2)]
