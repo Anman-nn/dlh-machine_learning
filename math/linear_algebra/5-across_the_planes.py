8b972065f2d7df8ca0a96f2203a851afeed891ed
Anman-nn <endricos@gmail.com>
2026-06-06 20:41:03 +0200

    task linear algebra

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,7 +4,7 @@
 
 def add_matrices2D(mat1, mat2):
     """add 2D matrices"""
-    if len(mat1) != len(mat2):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     res_mat = []
     for row1, row2 in zip(mat1, mat2):
