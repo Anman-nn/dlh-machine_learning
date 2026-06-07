a8525b008f2bf9f85d55cecfaeb198d099ee1360
Anman-nn <endricos@gmail.com>
2026-06-07 17:29:49 +0200

    task linear algebra

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -3,10 +3,4 @@
 
 
 def np_elementwise(mat1, mat2):
-    import numpy as np
-    return tuple(
-        mat1 + mat2,
-        mat1 - mat2,
-        mat1 * mat2,
-        mat1 / mat2
-    )
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
