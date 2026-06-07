52bad36dfa58339410464e71aa4098369a5fc90d
Anman-nn <endricos@gmail.com>
2026-06-07 17:41:42 +0200

    task linear algebra

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Documentatio of the module"""
 
+import numpy as np
+
 
 def np_matmul(mat1, mat2):
     """documentation function"""
-    import numpy as np
     return np.matmul(mat1, mat2)
