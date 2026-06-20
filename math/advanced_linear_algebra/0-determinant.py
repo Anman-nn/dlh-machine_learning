f664d433ecce2abd16ba4b5cfb03ce0e5e081dfb
Anman-nn <endricos@gmail.com>
2026-06-20 09:45:10 +0200

    task new

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''Module of Advanced Linear Algebra'''
+
+import numbers as np
+
+
+def determinant(matrix):
+    '''calculates the determinant of a matrix'''
+
+    if matrix == [[]]:
+        return 1
+
+    if not isinstance(matrix, list):
+        raise TypeError('matrix must be a list of lists')
+
+    mat = np.asarray(matrix)
+
+    if mat.ndim != 2:
+        raise TypeError('matrix must be a list of lists') 
+
+    if mat.shape[0] != mat.shape[1]:
+        raise ValueError('matrix must be a square matrix')
+
+    return round(np.linalg.det(mat))
