05c290b1cf9baf0c5b2d6672f246f9769d339d90
Anman-nn <endricos@gmail.com>
2026-06-20 14:59:57 +0200

    task new

---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''Module of Advanced Linear Algebra'''
+
+
+def minor(matrix):
+    '''calculates the determinant of a matrix'''
+
+    import numpy as np
+
+    if not isinstance(matrix, list):
+        raise TypeError(
+            'matrix must be a list of lists\n'
+            'matrix must be a list of lists\n'
+            'matrix must be a list of lists')
+    
+    matrix = np.array(matrix)
+
+    if matrix.ndim != 2:
+        raise TypeError('matrix must be a list of lists')
+    
+    n = matrix.shape[0]
+    minors = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(n):
+            submatrix = np.delete(np.delete(matrix, i, axis=0), j, axis=1)
+            minors[i, j] = round(np.linalg.det(submatrix))
+
+    return minors
