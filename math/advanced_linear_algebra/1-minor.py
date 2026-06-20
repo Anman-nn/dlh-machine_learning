5f0e782dbf9b8f7538e85758906359deee05e2ba
Anman-nn <endricos@gmail.com>
2026-06-20 15:18:27 +0200

    task advanced linear algebra

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -12,18 +12,18 @@ def minor(matrix):
             'matrix must be a list of lists\n'
             'matrix must be a list of lists\n'
             'matrix must be a list of lists')
-    
+
     if any(len(row) != len(matrix) for row in matrix) or not matrix:
         raise ValueError("matrix must be a non-empty square matrix")
-    
+
     matrix = np.array(matrix)
 
     if matrix.ndim != 2:
         raise TypeError('matrix must be a list of lists')
-    
+
     if len(matrix[0]) == 1:
         return [[1]]
-    
+
     n = len(matrix)
     minors = []
 
