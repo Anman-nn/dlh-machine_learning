ee00e457c34db2baacb5eab637dc0ad69aa53212
Anman-nn <endricos@gmail.com>
2026-06-20 10:48:43 +0200

    task advanced linear algebra

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -2,7 +2,6 @@
 '''Module of Advanced Linear Algebra'''
 
 
-
 def determinant(matrix):
     '''calculates the determinant of a matrix'''
 
@@ -12,7 +11,8 @@ def determinant(matrix):
         return 1
 
     if not isinstance(matrix, list):
-        raise TypeError('matrix must be a list of lists\nmatrix must be a list of lists\nmatrix must be a list of lists')
+        raise TypeError('matrix must be a list of lists\n' \
+        'matrix must be a list of lists\nmatrix must be a list of lists')
 
     if any(len(row) != len(matrix) for row in matrix):
         raise ValueError("matrix must be a square matrix")
