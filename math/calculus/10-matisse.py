1a57d2b172e0e1169faf2066311fae7fdd45bbdf
Anman-nn <endricos@gmail.com>
2026-07-06 22:42:41 +0200

    task calculus

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,7 +4,11 @@
 
 def poly_derivative(poly):
     '''function poly_derivative'''
+    if not isinstance(poly, list):
+        return None
+    if poly[1] == 0:
+        return 0
     res = []
     for i in range(1,len(poly)):
-      res.append(i*poly[i])
+        res.append(i*poly[i])
     return res
