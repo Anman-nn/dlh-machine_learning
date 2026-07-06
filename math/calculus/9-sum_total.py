130fcc3112b0a8ff043062474c20576832884314
Anman-nn <endricos@gmail.com>
2026-07-06 21:22:05 +0200

    task calculus

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+'''Module for project Calculus'''
+
+
 def summation_i_squared(n):
+    '''function summation_i_squared'''
+
     if n < 1:
         return None
-    
+
     return n * (n + 1) * (2 * n + 1) // 6
