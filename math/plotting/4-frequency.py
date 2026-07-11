9a5ad8f2de0638665421c8670117b7e54857b076
Anman-nn <endricos@gmail.com>
2026-07-11 16:31:28 +0200

    task plotting

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Draw a line'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=10, edgecolor='black')
+    plt.title('Project A')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
