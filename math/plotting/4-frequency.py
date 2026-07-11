c05a18089b0361394d3ea7c037bcdb48d99b17b0
Anman-nn <endricos@gmail.com>
2026-07-11 16:42:37 +0200

    task plotting

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -6,11 +6,14 @@ import matplotlib.pyplot as plt
 
 
 def frequency():
-
+    '''Draw a line'''
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, bins=10, edgecolor='black')
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
     plt.title('Project A')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
+    plt.xticks(range(0, 101, 10))
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
