147e93b7183376901ee29b2581c340a946e83ff6
Anman-nn <endricos@gmail.com>
2026-07-18 10:21:06 +0200

    task plotting

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -27,6 +27,7 @@ class Poisson:
         for i in range(1, k + 1):
             fact *= i
         return (self.lambtha ** k) * (2.7182818285 ** (-self.lambtha)) / fact
+
     def cdf(self, k):
         '''calculates CDF'''
         k = int(k)
