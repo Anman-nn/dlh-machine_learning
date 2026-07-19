8e75700b64d5d5dc5cf2f7b73e3cd6288802a41b
Anman-nn <endricos@gmail.com>
2026-07-19 10:33:01 +0200

    task plotting

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -22,4 +22,4 @@ class Exponential:
         if x < 0:
             return 0
 
-        return 1 - 2.7182818285 ** ((-self.lambtha) * x)
+        return self.lambtha * 2.7182818285 ** (-self.lambtha * x)
