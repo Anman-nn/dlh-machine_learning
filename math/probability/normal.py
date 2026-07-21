d756c3d920e8e8b0109adced22c420dc9c30926f
Anman-nn <endricos@gmail.com>
2026-07-21 20:43:09 +0200

    task plotting

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -19,6 +19,6 @@ class Normal:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
 
-        self.mean = sum(data) / len(data)
-        variance = sum((x - mean) ** 2 for x in data) / len(data)
-        self.stddev = variance ** 0.5
+            self.mean = sum(data) / len(data)
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+            self.stddev = variance ** 0.5
