1d30f5295fae971d4bb250e282ab7c21fa3b1f5d
Anman-nn <endricos@gmail.com>
2026-07-21 20:32:15 +0200

    task plotting

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 '''Module for the project Probability'''
 
-import numpy as np
 
 class Normal:
     '''Define a class Normal'''
-
 
     def __init__(self, data=None, mean=0., stddev=1.):
         self.mean = mean
@@ -14,4 +12,10 @@ class Normal:
             raise ValueError('stddev must be a positive value')
         if data is None:
             return (mean, stddev)
-        return (np.mean(data), np.std(data))
+        if not isinstance(data, list):
+            raise TypeError('data must be a list')
+
+        mean = sum(data) / len(data)
+        variance = sum((x - mean) ** 2 for x in data) / len(data)
+        stddev = variance ** 0.5
+        return (mean, stddev)
