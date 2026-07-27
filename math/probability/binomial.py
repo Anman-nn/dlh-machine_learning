03d95a4cbe375b9e9e9bb14fa6ad7e2e95194211
Anman-nn <endricos@gmail.com>
2026-07-27 21:12:36 +0200

    task plotting

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -8,22 +8,24 @@ class Binomial:
     def __init__(self, data=None, n=1, p=0.5):
 
         if data is None:
-            self.n = int(n)
             if n <= 0:
                 raise ValueError('n must be a positive value')
             if not 0 < p < 1:
                 raise ValueError('p must be greater than 0 and less than 1')
-            self.p = p
-        elif not isinstance(data, list):
-            raise TypeError("data must be a list")
-        if len(data) < 2:
-            raise ValueError("data must contain multiple values")
+            self.n = int(n)
+            self.p = float(p)
+
         else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
             mean = sum(data) / len(data)
             variance = sum((x - mean) ** 2 for x in data) / len(data)
 
             p = 1 - (variance / mean)
             n = round(mean / p)
             p = mean / n
-            self.n = n
-            self.p = p
+            self.n = int(n)
+            self.p = float(p)
