570317fff036d414300ae7c5046bb099a139415a
Anman-nn <endricos@gmail.com>
2026-08-03 22:37:15 +0200

    task plotting

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+'''Module for the bayesian Probability'''
+
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    '''function ldkfglkdsfjgskdnfk.gfds'''
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    n_fact = 1
+    for i in range(1, n + 1):
+        n_fact *= i
+
+    x_fact = 1
+    for i in range(1, x + 1):
+        x_fact *= i
+
+    nx_fact = 1
+    for i in range(1, n - x + 1):
+        nx_fact *= i
+
+    c = n_fact / (x_fact * nx_fact)
+
+    res = []
+    for p in P:
+        q = 1-p
+        res.append(c*p**x*q**(n-x))
+    return np.array(res)
+
+
+def intersection(x, n, P, Pr):
+    '''sdfjnsbdfmhsbfhdsdf'''
+    intersection = likelihood(x, n, P) * Pr
+    return intersection
