ec8068d005b05b3fd219f3648cf0913c89e34409
Anman-nn <endricos@gmail.com>
2026-08-16 11:10:40 +0200

    task probability

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -9,7 +9,7 @@ def correlation(C):
     '''function documented'''
 
     if not isinstance(C, np.ndarray):
-        raise TypeError("C must be a numpy.ndarra")
+        raise TypeError("C must be a numpy.ndarray")
 
     if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
