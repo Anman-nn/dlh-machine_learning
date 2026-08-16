d20c8281a401358ed55f2c59eed514c2f3450039
Anman-nn <endricos@gmail.com>
2026-08-16 16:50:12 +0200

    task probability

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -5,5 +5,6 @@ import pandas as pd
 
 
 def from_numpy(array):
+    '''FUNCTION is now documented'''
     colnames = [chr(65+i) for i in range(array.shape[1])]
     return pd.DataFrame(array, columns=colnames)
