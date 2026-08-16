956eed02d4483afe756eb88fdf79c04858ef624b
Anman-nn <endricos@gmail.com>
2026-08-16 16:49:26 +0200

    task probability

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+'''Module Pandas in the best!!!'''
+
 import pandas as pd
+
+
 def from_numpy(array):
     colnames = [chr(65+i) for i in range(array.shape[1])]
-    return pd.DataFrame(array, columns = colnames)
+    return pd.DataFrame(array, columns=colnames)
