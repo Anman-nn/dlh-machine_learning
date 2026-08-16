fe1209240e067ee5d63df326bf66a96a4f44b060
Anman-nn <endricos@gmail.com>
2026-08-16 10:55:56 +0200

    task probability

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -5,4 +5,17 @@
 import numpy as np
 
 def mean_cov(X):
-    
+    #if not isinstance(X, np.ndarray) or X.ndim != 2:
+       # raise TypeError("X must be a 2D numpy.ndarray")
+
+    n = X.shape[0]
+
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0, keepdims=True)
+
+    X_centered = X - mean
+    cov = (X_centered.T @ X_centered) / (n - 1)
+
+    return mean, cov
