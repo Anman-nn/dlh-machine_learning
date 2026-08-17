e7bf8db7b9b9a4f3834452fa509481c3df414c51
Anman-nn <endricos@gmail.com>
2026-08-17 17:35:53 +0200

    task probability

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-'''Module Pandas in the best!!!'''
-
-import pandas as pd
+'''Module Pandas is the best!!!'''
 
 
 def array(df):
+    import pandas as pd
     last10 = df.iloc[-10:]
     return last10.to_numpy()
