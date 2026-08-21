7388d95da0ba9c047ef5507f827b84605eb80379
Anman-nn <endricos@gmail.com>
2026-08-21 16:09:10 +0200

    task probability

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -3,13 +3,14 @@
 
 import pandas as pd
 
+
 def concat(df1, df2):
     '''dsfsdfdsfsdf'''
     index = __import__('10-index').index
     i_df1 = index(df1)
     i_df2 = index(df2)
     i_df2 = i_df2.loc[:1417411920]
-    return pd.concat([i_df2, i_df1], 
+    return pd.concat([i_df2, i_df1],
                      axis=0,
                      keys=['bitstamp', 'coinbase']
-    )
+                    )
